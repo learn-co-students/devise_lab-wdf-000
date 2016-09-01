4114f500a10ff2c770df3497f27965b659613dff
Margaret <eutopia@gmail.com>
2016-09-01 15:10:28 -0400

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def magic(sentence)
+		tmp = sentence.split(" ")
+		first = tmp.shift
+		a = first + " " + tmp.map(&:downcase).join(" ")
+		a.gsub(/! y/ ,"! Y")
+	end
 end
